7b7173d6fac5330598361a8a26b1bcfff921a1e7
Your Name <you@example.com>
2026-04-16 16:05:42 +0800

    优化方便测试

--- dapp-h5/src/api/http.ts
+++ dapp-h5/src/api/http.ts
@@ -2,8 +2,7 @@ import axios from 'axios'
 import type { ApiResponse } from '../types/api'
 
 const http = axios.create({
-  // baseURL: 'https://dhbapi.88888888.mom/api',
-  baseURL: '  http://test.robot.xyz/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 15000,
 })
 
